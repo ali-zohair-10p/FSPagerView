0f019d236e8d810b86d98cc230e0470560528a9d
Ali Zohair <>
2026-02-19 13:55:06 +0500

    rename package name due to conflict with class

--- Package.swift
+++ Package.swift
@@ -2,15 +2,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "FSPagerView",
+    name: "FSPagerViewKit",
     platforms: [
         .iOS(.v12)
     ],
     products: [
-        .library(name: "FSPagerView", targets: ["FSPagerView"]),
+        .library(name: "FSPagerViewKit", targets: ["FSPagerViewKit"]),
     ],
     targets: [
-        .target(name: "FSPagerView", path: "Sources", exclude: ["FSPagerViewObjcCompat.h", "FSPagerViewObjcCompat.m"]),
+        .target(name: "FSPagerViewKit", path: "Sources", exclude: ["FSPagerViewObjcCompat.h", "FSPagerViewObjcCompat.m"]),
     ],
     swiftLanguageVersions: [.v5]
 )
